f6d975fc014fe2d5eb884d0f4d81f98861aeab2d
Rohit Karande <rohitkarande543@gmail.com>
2022-10-21 17:22:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,110 +1,93 @@
-import React, { Component, useEffect, useState } from "react";
+import React, {Component, useEffect, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
 
   const [getWorkDuration,setWorkDuration] = useState(25);
   const [getBreakDuration,setBreakDuration] = useState(5);
-  const [getWorkInSeconds,setWorkInSeconds] = useState(25*60);
-  const [getBreakInSeconds,setBreakInSeconds] = useState(25*60);
-
-  const [getType,setType] = useState('');
+  const [getSecondsRecordsWork,setSecondsRecordsWork] = useState(0);
+  const [getSecondsRecordsBreak,setSecondsRecordsBreak] = useState(0);
   const [getFlag,setFlag] = useState(false);
-  let workResult;
-  let breakResult;
+  const [getType,settype] = useState("");
 
   useEffect(()=>{
- 
-    if(getType == "work"  && getFlag && getWorkInSeconds>=1){
-      workResult = setTimeout(()=>{
-          setWorkInSeconds(getWorkInSeconds-1);
-        },1000)
-    }
 
-    if(getType == "work"  && getFlag && getWorkInSeconds<1){
-      setType('break');
-      clearTimeout(workResult);
-    }
-
-    if(getType == 'break' && getFlag && getBreakInSeconds>=1){
-      breakResult = setTimeout(()=>{
-        setBreakInSeconds(getBreakInSeconds-1);
+    if(getType == "work"){
+      const result = setTimeout(()=>{
+        console.log(getSecondsRecordsWork);
+        setSecondsRecordsWork(getSecondsRecordsWork-1);
       },1000)
+  
+      return ()=> clearTimeout(result);
     }
+  
 
-    if(getType == "break"  && getFlag && getBreakInSeconds<1){
-      setType('');
-      clearTimeout(breakResult);
-      setFlag(false);
-    }
-    
-  },[getType,getFlag,getWorkInSeconds])
+  },[getType,getSecondsRecordsWork])
 
-  const onChangeDurationHandler=(event)=>{
-    if(event.target.name == 'work'){
-      setWorkDuration(parseInt(event.target.value)<0?'':parseInt(event.target.value));
-    }
-    else{
-      setBreakDuration(parseInt(event.target.value)<0?'':parseInt(event.target.value));
-    }
+  const onDurationCheck=(event)=>{
+    console.log(typeof event.target.value);
+        if(event.target.name === 'work-duration'){
+          if(parseInt(event.target.value<0)){
+            setWorkDuration('');
+          }
+          else{
+            setWorkDuration(parseInt(event.target.value));
+          }
+        }
+        else{
+          if(parseInt(event.target.value<0)){
+            setBreakDuration('');
+          }
+          else{
+            setBreakDuration(parseInt(event.target.value));
+          }
+        }    
   }
 
-  const onStartHandler=()=>{
-    setType('work');
+  const onSetHandler=()=>{
+    alert("coming");
     setFlag(true);
-    setWorkInSeconds(getWorkDuration*60);
-    setBreakInSeconds(getBreakDuration*60);
+    settype("work");
+    console.log(getWorkDuration);
+    console.log(getWorkDuration * 60);
+    setSecondsRecordsWork(getWorkDuration*60);
   }
 
   const onStopHandler=()=>{
-    if(workResult){
-      clearTimeout(workResult);
-    }
-    if(breakResult){
-      clearTimeout(breakResult);
-    }
+     setFlag(false);
   }
 
   const onResetHandler=()=>{
-    setType('');
-    setFlag(false);
-    if(workResult){
-      clearTimeout(workResult);
-    }
-    if(breakResult){
-      clearTimeout(breakResult);
-    }
-    setWorkDuration(25);
-    setBreakDuration(5);
+       setFlag(false);
   }
 
-  const setTimeFormat=(input)=>{
-    let m = parseInt(input/60).toString();
-    let s = parseInt(input%60).toString();
-    let minutesInput = m.length<=1?"0"+m:m;
-    let secondsInput = s.length<=1?"0"+s:s;
-    console.log(secondsInput);
-    return `${minutesInput}:${secondsInput}`;
+  const getHandlers=(value)=>{
+     let m = parseInt(value/60).toString();
+     let s = parseInt(value%60).toString();
+     let input = (m.length>1)?m: "0"+m;
+     let inputTwo = (s.length>1)?s:"0"+s;
+     return `${input}:${inputTwo}`;
   }
+
 
   return (
     <div id="main">
-      <div className="timer">
-      {getType == 'work' && getFlag && setTimeFormat(getWorkInSeconds)}
-      {getType == 'break' && getFlag && setTimeFormat(getBreakInSeconds)}
+
+      <div className="container">
+          {getType === "work"? getHandlers(getSecondsRecordsWork):""}
+          <br/>
+               {getType}
       </div>
-      <div className="timer">
-        {getType == 'work'? 'Work-Time': 'Break-Time'}
+      <div className="container">
+      <button disabled={getFlag} onClick={onSetHandler} data-testid='start-btn'>start</button>
+      <button disabled={!getFlag} onClick={onStopHandler} data-testid='stop-btn'>stop</button>
+      <button disabled={!getFlag} onClick={onResetHandler} data-testid='reset-btn'>reset</button>
       </div>
-      <div className="timer">
-        <button disabled={getFlag}  data-testid='start-btn' onClick={onStartHandler}>Start</button>
-        <button disabled={!getFlag} data-testid='stop-btn' onClick={onStopHandler}>Stop</button>
-        <button disabled={!getFlag}  data-testid='reset-btn' onClick={onResetHandler}>Reset</button>
-      </div>
-      <div className="timer">
-        <input type="number" value={getWorkDuration} name="work" onChange={onChangeDurationHandler} data-testid='work-duration'/>
-        <input type="number" value={getBreakDuration} name="break" onChange={onChangeDurationHandler}  data-testid='break-duration'/>
-        <button disabled={getType!="" && getFlag}   data-testid='set-btn'>Set</button>
+
+      <div className="container">
+        <input type="number" value={getWorkDuration} name="work-duration" onChange={onDurationCheck} data-testid='work-duration'/>
+        <input type="number" value={getBreakDuration} name="break-duration" onChange={onDurationCheck} data-testid='break-duration'/>
+        <button  disabled={getFlag} onClick={onSetHandler} data-testid='set-btn' >Set</button>
       </div>
     </div>
   )
